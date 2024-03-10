326a23abea5c72f4835eb27ece03247cf6017809
Nata Nael <natanael280198@gmail.com>
2024-03-10 18:06:09 +0700

    feat: updated tsconfig and added solid library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [
+    preact({
+      compat: true,
+    }),
+    solidJs(),
+  ],
 });
